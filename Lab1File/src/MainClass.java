d87e46015651b6e43c65d3e84205673402e63b8b
Priya Kaviyil <L00171183@atu.ie>
2022-11-09 14:08:12 +0000

    Files added

---
+++ Lab1File/src/MainClass.java
@@ -0,0 +1,14 @@
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		
+		System.out.println("Hey");
+		
+		System.out.println("Hi");
+		
+	}
+
+}
